5021ae0060df59e930064aa6c599df2068fb6730
srbm <shawnrbm@gmail.com>
2019-06-18 16:24:58 -0700

    added delete functionality

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-$(
-    function handleSubmit() {
-        
-        $('#js-shopping-list-form').submit(e => {
-            e.preventDefault();
-            const listItem = $('#shopping-list-entry').val();
-            console.log(listItem);
-            $('.shopping-list').append(`
-            <li>
-                <span class="shopping-item">${listItem}</span>
-                <div class="shopping-item-controls">
-                    <button class="shopping-item-toggle">
-                        <span class="button-label">check</span>
-                    </button>
-                    <button class="shopping-item-delete">
-                        <span class="button-label">delete</span>
-                    </button>
-                </div>
-            </li>`)
-        });
-    }
-)
+$(function handleSubmit() {
+    $('#js-shopping-list-form').submit(e => {
+        e.preventDefault();
+        const listItem = $('#shopping-list-entry').val();
+        $('.shopping-list').append(`
+        <li>
+            <span class="shopping-item">${listItem}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`)
+    })
+});
+$(function handleDeleteButton() {
+    $('.shopping-item-delete').click(e => {
+        console.log($(e.currentTarget).closest('.shopping-item'));
+        $(e.currentTarget).closest('li').remove();
+    });
+});
